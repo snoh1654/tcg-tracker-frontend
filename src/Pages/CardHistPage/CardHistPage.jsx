9849827f3b30308feaf1b85688e4495c67d3ffe3
Sean Noh <seannoh1654@yahoo.ca>
2025-05-18 00:17:51 -0700

    Improved UI design for history buttons

--- src/Pages/CardHistPage/CardHistPage.jsx
+++ src/Pages/CardHistPage/CardHistPage.jsx
@@ -1,23 +1,20 @@
 import "./CardHistPage.css";
 import BackButton from "../../Components/BackButton/BackButton.jsx";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import CardHistComponent from "../../Components/CardHistComponent/CardHistComponent.jsx";
 import { useLocation } from "react-router-dom";
 
+const activeButtonType = {
+  "2W": 14,
+  "3M": 90,
+  "1Y": 365,
+};
+
 function CardHistPage() {
   const [cardHist, setCardHist] = useState([]);
   const { tcg_name, set_name, card_id } = useParams();
-
-  const location = useLocation();
-  const initialRecordDate = location.state.initial_record_date;
-  if (initialRecordDate) {
-    const maxDateDiff = Math.floor(
-      (Date.now() - new Date(initialRecordDate.replace(/\.\d+/, ""))) /
-        (1000 * 60 * 60 * 24)
-    );
-  }
-
+  const url = import.meta.env.VITE_API_URL + "card/" + set_name + "/" + card_id;
   const imageSrc =
     import.meta.env.VITE_CLOUDFRONT_URL +
     tcg_name +
@@ -26,14 +23,26 @@ function CardHistPage() {
     "/" +
     card_id +
     ".jpg";
-  const url = import.meta.env.VITE_API_URL + "card/" + set_name + "/" + card_id;
+  const activeButton = useRef("2W");
+
+  const location = useLocation();
+  const initialRecordDate = location.state.initial_record_date;
+
+  if (initialRecordDate) {
+    activeButtonType["ALL"] = Math.floor(
+      (Date.now() - new Date(initialRecordDate.replace(/\.\d+/, ""))) /
+        (1000 * 60 * 60 * 24)
+    );
+  }
+
   useEffect(() => {
     fetchCardHist(url);
   }, []);
 
-  async function updatePriceHistory(days_since_present) {
-    console.log(days_since_present);
+  async function updatePriceHistory(buttonType) {
+    let days_since_present = activeButtonType[buttonType];
     let fetchURL = url + `?start_date=${days_since_present}`;
+    activeButton.current = buttonType;
     fetchCardHist(fetchURL);
   }
 
@@ -52,26 +61,34 @@ function CardHistPage() {
           <CardHistComponent cardHist={cardHist} />
           <div className="hist-buttons">
             <button
-              onClick={() => updatePriceHistory(14)}
-              className="hist-button two-weeks"
+              onClick={() => updatePriceHistory("2W")}
+              className={`hist-button two-weeks ${
+                activeButton.current === "2W" ? "active" : ""
+              }`}
             >
               Two Weeks
             </button>
             <button
-              onClick={() => updatePriceHistory(90)}
-              className="hist-button three-months"
+              onClick={() => updatePriceHistory("3M")}
+              className={`hist-button three-months ${
+                activeButton.current === "3M" ? "active" : ""
+              }`}
             >
               Three Months
             </button>
             <button
-              onClick={() => updatePriceHistory(365)}
-              className="hist-button one-year"
+              onClick={() => updatePriceHistory("1Y")}
+              className={`hist-button one-year ${
+                activeButton.current === "1Y" ? "active" : ""
+              }`}
             >
               One Year
             </button>
             <button
-              onClick={() => updatePriceHistory(maxDateDiff)}
-              className="hist-button all-time"
+              onClick={() => updatePriceHistory("ALL")}
+              className={`hist-button all-time ${
+                activeButton.current === "ALL" ? "active" : ""
+              }`}
               disabled={initialRecordDate === undefined}
             >
               All Time
