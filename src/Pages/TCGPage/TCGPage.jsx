c3bb99df58ea558e91c6af211f98a8a9489cf4dd
Sean Noh <seannoh1654@yahoo.ca>
2025-05-18 23:32:26 -0700

    Changed img src for public images

--- src/Pages/TCGPage/TCGPage.jsx
+++ src/Pages/TCGPage/TCGPage.jsx
@@ -5,8 +5,8 @@ function TCGPage() {
   return (
     <>
       <div className="tcg-container">
-        <TCGComponent name="One Piece Card Game" img="public\optcg_logo.png" />
-        <TCGComponent name="Weiss Schwarz" img="public\weiss_logo.png" />
+        <TCGComponent name="One Piece Card Game" img="optcg_logo.png" />
+        <TCGComponent name="Weiss Schwarz" img="weiss_logo.png" />
       </div>
     </>
   );
