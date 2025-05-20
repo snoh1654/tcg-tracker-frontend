2c479d297f98ba37f08c0a02ab7056cb18b55b5a
Sean Noh <seannoh1654@yahoo.ca>
2025-05-19 23:58:39 -0700

    Included Pokemon in tracking

--- src/Pages/TCGPage/TCGPage.jsx
+++ src/Pages/TCGPage/TCGPage.jsx
@@ -5,6 +5,7 @@ function TCGPage() {
   return (
     <>
       <div className="tcg-container">
+        <TCGComponent name="Pokemon" img="pokemon_logo.png" />
         <TCGComponent name="One Piece Card Game" img="optcg_logo.png" />
         <TCGComponent name="Weiss Schwarz" img="weiss_logo.png" />
       </div>
