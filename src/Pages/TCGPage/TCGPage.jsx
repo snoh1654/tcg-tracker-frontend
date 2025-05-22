51ee5f129ff74965bc41128add7b7385cf318771
Sean Noh <seannoh1654@yahoo.ca>
2025-05-21 23:53:38 -0700

    Updated TCG Page Images and Spacing

--- src/Pages/TCGPage/TCGPage.jsx
+++ src/Pages/TCGPage/TCGPage.jsx
@@ -5,8 +5,10 @@ function TCGPage() {
   return (
     <>
       <div className="tcg-container">
-        <TCGComponent name="Pokemon" img="pokemon_logo.png" />
         <TCGComponent name="One Piece Card Game" img="optcg_logo.png" />
+        <TCGComponent name="Pokemon" img="pokemon_logo.png" />
+        <TCGComponent name="Yugioh" img="yugioh_logo.png" />
+        <TCGComponent name="Cardfight Vanguard" img="cfv_logo.png" />
         <TCGComponent name="Weiss Schwarz" img="weiss_logo.png" />
       </div>
     </>
