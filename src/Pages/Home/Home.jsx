4ba97d1407904a72b94f42706c929431799010b2
Sean Noh <seannoh1654@yahoo.ca>
2025-05-21 20:55:11 -0700

    Included source to github repos in home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import "./Home.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function Home() {
   return (
@@ -15,6 +17,36 @@ function Home() {
       <Link to="cards" className="button">
         View Price History!
       </Link>
+
+      <div className="links-container">
+        <div className="github-container">
+          Web App Repo
+          <a
+            href="https://github.com/snoh1654/tcg-tracker-frontend"
+            className="github-link"
+            target="_blank"
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              className="github-icon"
+              target="_blank"
+            ></FontAwesomeIcon>
+          </a>
+        </div>
+        <div className="github-container">
+          AWS Backend Repo
+          <a
+            href="https://github.com/snoh1654/tcg-tracker-aws"
+            className="github-link"
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              className="github-icon"
+              target="_blank"
+            ></FontAwesomeIcon>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
