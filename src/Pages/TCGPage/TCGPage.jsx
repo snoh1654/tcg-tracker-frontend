859f88854ac845f3b41bad84a7d37c892c8d4cdf
Sean Noh <seannoh1654@yahoo.ca>
2025-05-22 22:07:23 -0700

    Fixed TCG page incorrect href bug

--- src/Pages/TCGPage/TCGPage.jsx
+++ src/Pages/TCGPage/TCGPage.jsx
@@ -7,7 +7,7 @@ function TCGPage() {
       <div className="tcg-container">
         <TCGComponent name="One Piece Card Game" img="optcg_logo.png" />
         <TCGComponent name="Pokemon" img="pokemon_logo.png" />
-        <TCGComponent name="Yugioh" img="yugioh_logo.png" />
+        <TCGComponent name="Yu-Gi-Oh!" img="yugioh_logo.png" />
         <TCGComponent name="Cardfight Vanguard" img="cfv_logo.png" />
         <TCGComponent name="Weiss Schwarz" img="weiss_logo.png" />
       </div>
